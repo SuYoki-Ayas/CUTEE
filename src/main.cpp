52b90888de266dad0c281c39b1923aa56ec0e2f7
SuYoki-Ayas <2167802858@qq.com>
2026-08-17 14:06:26 +0800

    更新 项目结构 更新 CMakeList.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
+#include <QApplication>
 #include "CUTEE.h"
-#include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     CUTEE window;
